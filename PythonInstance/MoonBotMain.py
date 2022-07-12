c2deb079a209785ca90458e1b4940614d1e91ead
Yui <mattmeiz99@gmail.com>
2022-07-12 21:52:54 +0200

    Update MoonBotMain.py

--- PythonInstance/MoonBotMain.py
+++ PythonInstance/MoonBotMain.py
@@ -93,7 +93,7 @@ for directory in os.listdir('./cogs'):
             if filename.endswith(".py"):
                 if filename != 'importantfunctions.py':
                     initial_extensions.append("cogs." + directory + '.' + filename[:-3])
-                    print(directory + "/" + filename[:-3] + ".py was loaded successfully")
+                    print(directory + "/" + filename[:-3] + " was loaded successfully")
 
 
 if __name__ == '__main__':
